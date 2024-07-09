5d076938ef4cf39e0d3fa4628c7296aed715517e
TKU-FCP-412637711 <412637711@o365.tku.edu.tw>
2024-07-09 13:47:27 +0800

    7/9下午

---
+++ ch5_1.java
@@ -0,0 +1,8 @@
+public class ch5_1 {
+    public static void main(String[] args) {
+        int a=4;
+        if(a%2==0){         //判別a除以2的餘數是否為0
+            System.out.printf("%d is an even number" , a);
+        }
+    }
+}
---
+++ ch5_2.java
@@ -0,0 +1,9 @@
+public class ch5_2 {
+    public static void main(String[] args) {
+        int a=15;
+        if (a%2==0)     //如果可被2整除
+            System.out.printf("%d is an even number",a);  //印出偶數
+            else
+            System.out.printf("%d is an odd number",a); //印出奇數
+    }
+}
