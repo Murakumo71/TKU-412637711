9d4298495a45fd527aab9a49739d28941c14ab2b
TKU-FCP-412637711 <412637711@o365.tku.edu.tw>
2024-07-15 11:53:29 +0800

    7/15 morning

---
+++ ch5_10.java
@@ -0,0 +1,12 @@
+public class ch5_10 {
+    public static void main(String[] args) {
+        int i;
+        for (i=1;i<=10;i++){
+            if(i%3==0)          //判斷i%3是否為0
+            break;
+        System.out.println("i="+i);         //印出i的值
+
+        }
+        System.out.println("When loop interrupted, i="+i);
+    }
+}
---
+++ ch5_5_1.java
@@ -0,0 +1,6 @@
+public class ch5_5_1 {
+    
+    public static void main(String[] args){
+        int i 
+    }
+}
